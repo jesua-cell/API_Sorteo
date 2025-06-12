68968db9e022874e2b24c9576f3bf6044c9058c1
Jesua <180544338+jesua-cell@users.noreply.github.com>
2025-06-12 11:52:44 -0400

    Obtencion de los datos de la BD en el inventatio de los admins en la Sesion.jsx

--- client/src/sesion/Sesion.jsx
+++ client/src/sesion/Sesion.jsx
@@ -1,57 +1,123 @@
-import  borrar  from "../assets/borrar.png";
-import  editar  from "../assets/editar.png";
+import { useEffect, useState } from "react";
+import borrar from "../assets/borrar.png";
+import editar from "../assets/editar.png";
+import zoom from "../assets/zoom.png";
+import axios from "axios";
 
 export const Sesion = () => {
+
+    const [jugadores, setJugadores] = useState([]);
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        const fetchJugadores = async () => {
+            try {
+                const response = await axios.get('http://localhost:3000/jugadores');
+                console.log("datos recibidos", response.data);
+                setJugadores(response.data);
+                setLoading(false);
+            } catch (error) {
+                console.error("Error al obtener jugadores en el Admin", error);
+                setLoading(false);
+            }
+        };
+        fetchJugadores();
+    }, [])
+
+    if (loading) {
+        return <div><h4>Cargando Jugadores...</h4></div>
+    }
+
     return (
         <>
             <div className="cont_admins">
                 <h2>Inventario</h2>
-                <div className="box_jugador">
-                    <dl className="lista-datos">
-                        <div className="fila">
-                            <dt>ID:</dt>
-                            <dd>0012</dd>
-                        </div>
-                        <div className="fila">
-                            <dt>Nombres:</dt>
-                            <dd><strong>Pepe Gonzales</strong></dd>
-                        </div>
-                        <div className="fila">
-                            <dt>Celular:</dt>
-                            <dd>302 4778569</dd>
-                        </div>
-                        <div className="fila">
-                            <dt>Cédula:</dt>
-                            <dd>123547895</dd>
-                        </div>
-                        <div className="fila">
-                            <dt>Método de Pago:</dt>
-                            <dd>Nequi</dd>
-                        </div>
-                        <div className="fila">
-                            <dt>Comprobante:</dt>
-                            <dd>imagen.png</dd>
-                        </div>
-                        <div className="fila">
-                            <dt>Referencia:</dt>
-                            <dd>REF5689</dd>
-                        </div>
-                        <div className="fila">
-                            <dt>Numeros de Boletos:</dt>
-                            <dd>
-                                <p className='fila_num'>0256</p>
-                                <p className='fila_num'>0936</p>
-                                <p className='fila_num'>0776</p>
-                            </dd>
-                        </div>
-                        <div className="fila">
-                            <dt>Fecha:</dt>
-                            <dd>2025-06-06 16:07:35</dd>
-                        </div>
-                    <button><img src={borrar} alt="Borrar" /></button>
-                    <button><img src={editar} alt="Borrar" /></button>
-                    </dl>
-                </div>
+                <input type="text"
+                    placeholder="Buscador..."
+                    className="buscador_sesion"
+                />
+
+                {jugadores.map(jugador => (
+
+                    <div className="box_jugador" key={jugador.id}>
+                        <dl className="lista-datos">
+                            <div className="fila">
+                                <dt>ID:</dt>
+                                <dd>{jugador.id}</dd>
+                            </div>
+                            <div className="fila">
+                                <dt>Nombres:</dt>
+                                <dd><strong>{jugador.nombres_apellidos}</strong></dd>
+                            </div>
+                            <div className="fila">
+                                <dt>Celular:</dt>
+                                <dd>{jugador.celular}</dd>
+                            </div>
+                            <div className="fila">
+                                <dt>Cédula:</dt>
+                                <dd>{jugador.cedula}</dd>
+                            </div>
+                            <div className="fila">
+                                <dt>Ciudad y Estado:</dt>
+                                <dd>{jugador.pais_estado}</dd>
+                            </div>
+                            <div className="fila">
+                                <dt>Método de Pago:</dt>
+                                <dd>{jugador.metodo_pago}</dd>
+                            </div>
+                            <div className="fila">
+                                <dt>Comprobante:</dt>
+                                <dd>
+                                    {jugador.comprobante_url ? (
+                                        <div className="cont_comprobante_img">
+                                            <a
+                                                className="targe_comprobante"
+                                                href={jugador.comprobante_url}
+                                                target="_blank"
+                                                rel="moopener noreferrer"
+                                            >
+                                                <img
+                                                    src={zoom}
+                                                    className="zoom_comprobante"
+                                                />
+                                                <img
+                                                    className="comprobante_img"
+                                                    src={jugador.comprobante_url}
+                                                    alt="Comprobante de pago"
+                                                />
+                                            </a>
+                                        </div>
+                                    ) : (
+                                        "Comprobante de pago"
+                                    )}
+                                </dd>
+                            </div>
+                            <div className="fila">
+                                <dt>Referencia:</dt>
+                                <dd>{jugador.referenciaPago}</dd>
+                            </div>
+                            <div className="fila">
+                                <dt>Numeros de Boletos:</dt>
+                                <dd>
+                                    {jugador.boletos && jugador.boletos.map((boleto, index) => (
+                                        <p className="fila_num" key={index}>{boleto}</p>
+                                    ))}
+                                </dd>
+                            </div>
+                            <div className="fila">
+                                <dt>Fecha:</dt>
+                                <dd><strong>{new Date(jugador.fecha_registro).toLocaleDateString()}</strong>
+                                    <br />
+                                    {new Date(jugador.fecha_registro).toLocaleTimeString()}
+                                </dd>
+                            </div>
+                            <div className="lista_btns">
+                                <button className="btn_eliminar_sesion"><img src={borrar} alt="Borrar" /></button>
+                                <button className="btn_editar_sesion"><img src={editar} alt="Borrar" /></button>
+                            </div>
+                        </dl>
+                    </div>
+                ))}
             </div>
         </>
     )
