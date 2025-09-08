6831ada2adf1d9c9d176bcdb528f825759a7161e
Jesua <180544338+jesua-cell@users.noreply.github.com>
2025-09-08 14:30:35 -0400

    Diseñando el archivo NotFound.jsx

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-
 export const NotFound = () => {
     return (
         <>
-            <h1>Error 404</h1>
+            <div className="ContErrorFound">
+                <h1 className='title_notFound'>NotFound 404</h1>
+                <p className="p_notFound">
+                    Pagina caida, enlace incorrecto o eliminado...
+                </p>
+            </div>
         </>
     )
 }
